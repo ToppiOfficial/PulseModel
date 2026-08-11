f471e243f8e746f32a3cdd0910922875dadfc89a
ToppiOfficial <toppi.official@gmail.com>
2026-08-11 12:08:14 +0800

    Revert a change.
    mdlcompiler:
    - Allow duplicated $declaresequence entries.
    - Bump version to 0.5.2

--- mdlcompiler/qcloader.cpp
+++ mdlcompiler/qcloader.cpp
@@ -1898,9 +1898,6 @@ bool CmdDeclareSequence(Ctx& c, const Token& cmd) {
     seq.fadeouttime = c.defaultFadeOut;
     if (!c.Want("a name", cmd, seq.name))
         return false;
-    for (const auto& s : c.in.sequences)
-        if (_stricmp(s.name.c_str(), seq.name.c_str()) == 0)
-            return c.Fail(cmd.line, "duplicate sequence name \"" + seq.name + "\"");
     seq.isDeclare = true;
     seq.flags |= kStudioOverride;
     c.in.sequences.push_back(std::move(seq));
